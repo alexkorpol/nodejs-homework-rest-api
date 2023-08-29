f158c97544de3a8307d0bde0f26f5f3b093c6b8c
Alexander <alexkorpol1964@gmail.com>
2023-08-29 10:27:28 +0300

    fix  path avatarURL

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -20,7 +20,7 @@ const updateAvatar = async (req, res) => {
   try {
     const resultUpload = path.join(avatarsDir, imageName);
     await fs.rename(tmpUpload, resultUpload);
-    const avatarURL = path.join('public', 'avatars', imageName);
+    const avatarURL = path.join('avatars', imageName);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.json({ avatarURL });
   } catch (error) {

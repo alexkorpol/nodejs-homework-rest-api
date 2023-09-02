45226223b4ba14aea532a8ec75e726c76e725d27
Alexander <alexkorpol1964@gmail.com>
2023-09-03 00:11:06 +0300

    Fix sendEmail.js
    Hidden EMAIL_OF_SENDER to .env

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,20 +1,18 @@
-const sgMail = require('@sendgrid/mail');
+const sendgrid = require('@sendgrid/mail');
 require('dotenv').config();
 
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, EMAIL_OF_SENDER } = process.env;
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+sendgrid.setApiKey(SENDGRID_API_KEY);
 
 async function sendEmail(data) {
-  const email = { ...data, from: 'alexkorpol1964@gmail.com' };
-    try {
-         
-            await sgMail.send(email);
-            return true;
-    } catch (error) {
-        throw error;
-     }
-};
+  const email = { ...data, from: EMAIL_OF_SENDER };
+  try {
+    await sendgrid.send(email);
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = sendEmail;
-
